bef02430420451510c7ccc658fd3b3f6ecef784a
David Talbot <dctalbot@umich.edu>
2017-05-20 17:36:41 -0400

    cleanup

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,6 @@
 
-function customflip() {
-
-}
-
-function rflip() {$('#card').flip();}
-
 // DRIVER
 $(function() {
-  // $('#card').on('mouseover', customflip);
-  // $('#card').on('mouseout', rflip);
-  // window.alert('s');
   $("#card").flip({
     axis: 'x',
     trigger: 'hover'
